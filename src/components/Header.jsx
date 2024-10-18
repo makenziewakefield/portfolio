39c02ae26c2b011b210601439a20a96b84a65dbf
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-10-17 20:45:33 -0600

    Initialize project files and begin adding content to header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Header.scss'
+
+const Header = () => {
+  return (
+    <section className="header-section">
+      <h1 className="header-name">Makenzie Wakefield</h1>
+    </section>
+  )
+}
+
+export default Header;
